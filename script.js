66e13ecafda6cdd59dd7c00d5259241786123773
Useryanis <yanistebe9@gmail.com>
2025-12-28 01:56:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const bouton = document.getElementById('boutonCommande');
+
+bouton.addEventListener('click', function() {
+    window.location.href = "https://wa.me/2290196353131?text=Bonjour, je veux commander un plat !";
+});
